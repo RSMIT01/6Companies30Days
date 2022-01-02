763fc818dbceaeb264cdefef66aa8e171334be80
Smitkumar Rathod <rathorvl97@gmail.com>
2022-01-02 14:25:53 +0530

    challenge started

---
+++ Goldman_Sachs/1.cpp
@@ -0,0 +1,19 @@
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+        //code here
+        map<string,vector<string>>m;
+        for(int i=0;i<string_list.size();i++)
+        {
+            string cur=string_list[i];
+            sort(cur.begin(),cur.end());
+            m[cur].push_back(string_list[i]);
+        }
+         vector<vector<string>>ans;
+        for(auto it:m)
+        {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
